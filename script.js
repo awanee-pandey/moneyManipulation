a7ed3ff6cd7a5d887e558a258c654c843d062cf3
“Awanee <Awanee.pandey@gmail.com>
2021-01-24 21:20:07 -0600

    final

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
 
+
+/* Format money */
+const formatMoney =(number)=>{
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
 /* Populate data into the DOM */
 const updateDOM = (resultData = data)=>{
-   main.innerHTML = '<h2><strong>Person</strong></h2>';
-   resultData.forEach(person=>{
-    const element =document.createElement('div');
+   main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+   resultData.forEach(personInfo => {
+    const element = document.createElement('div');
     element.classList.add('person');
-    element.innerHTML =`<strong>${person.name}</strong>${person.money}`;
-   })
-   console.log(resultData);
+    element.innerHTML =`<strong>${personInfo.name}</strong>${formatMoney(personInfo.money)}`;
+    main.appendChild(element); 
+  })
 } 
 
 /* Add objects into an array */
@@ -43,4 +49,39 @@ getRandomUser();
 getRandomUser();
 
 
+/* Double the money */
+const doubleMoney = () =>{
+  data = data.map(user => {
+    return {...user, money:user.money * 2};
+  })
 
+  updateDOM();
+}
+
+/* Show millionare */
+const showMillionare = ()=>{
+  data = data.filter(user => user.money > 1000000)
+  updateDOM();
+}
+
+/* Sort */
+const sortByRichest = () =>{
+  data.sort((low,high)=>high.money-low.money);
+  updateDOM();
+}
+
+const totalWealth = () =>{
+  const total = data.reduce((acc, user) => (acc + user.money), 0);
+
+  const element = document.createElement('div');
+  element.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    total
+  )}</strong></h3>`;
+  main.appendChild(element);
+}
+
+addUserBtn.addEventListener('click',getRandomUser);
+doubleBtn.addEventListener('click',doubleMoney);
+showMillionairesBtn.addEventListener('click',showMillionare);
+sortBtn.addEventListener('click',sortByRichest);
+calculateWealthBtn.addEventListener('click',totalWealth);
